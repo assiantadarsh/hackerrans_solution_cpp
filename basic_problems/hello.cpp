363d62a1f0bdebf28b81a37f7952c4585820875f
Adarsh shukla <adarshbhai917963@gmail.com>
2026-04-06 21:09:55 +0530

    Create hello.cpp

---
+++ basic_problems/hello.cpp
@@ -0,0 +1,12 @@
+// Platform -> Hackerrank]
+// Problem -> Say hello world
+// Language -> C++
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!";
+    return 0;
+}
