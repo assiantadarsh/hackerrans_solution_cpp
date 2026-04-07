3923c7462f5eac28db74b7fdfe486e38f45d126f
Adarsh shukla <adarshbhai917963@gmail.com>
2026-04-07 21:52:47 +0530

    Modify pointer.cpp for new problem and implementation
    Updated the problem statement and added a new implementation for updating two integers using pointers.

---
+++ function_problem/pointer.cpp
@@ -0,0 +1,22 @@
+// Platform -> Hackerrank
+// problem -> Add number 
+// Language -> Cpp
+#include <iostream>
+using namespace std;
+int update(int *a,int *b) {
+    int temp = *a;
+    *a = *a + *b;
+    *b = abs(temp - *b);
+    return *a,*b;
+}
+
+int main() {
+    int a, b;
+    cin>>a>>b;
+    int *pa = &a, *pb = &b;
+    
+    update(pa, pb);
+    cout<<a<<"\n"<<b;
+    
+    return 0;
+}
