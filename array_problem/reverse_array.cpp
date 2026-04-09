0f704f0dadb8dcaa0f5ab39634d990f9977233a8
Adarsh shukla <adarshbhai917963@gmail.com>
2026-04-09 22:43:52 +0530

    Create reverse_array.cpp

---
+++ array_problem/reverse_array.cpp
@@ -0,0 +1,35 @@
+// Platform -> Hackerrank
+// Problem -> Reverse the array without using extra loop
+// Language -> cpp
+// Time complexity -> O(logn)
+// Space complexity -> O(n)
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int n;
+   cin>>n;
+   int arr[n];
+   
+   for(int i = 0;i<n;i++){
+    cin>>arr[i];
+   }
+   
+   for(int i = 0; i<n/2; i++){
+    int temp = arr[i];
+    arr[i] = arr[n - 1 - i];
+    arr[n - 1 - i] = temp;
+
+   }
+   for(int i = 0;i<n;i++){
+    cout<<arr[i]<<" ";
+   }
+   
+    return 0;
+}
